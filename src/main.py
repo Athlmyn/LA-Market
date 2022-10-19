ccd42909d5b9d8ae44f410553d734bee5a568310
Athlmyn <Athlmyn@Outlook.com>
2022-10-19 01:09:27 -0600

    initial commit 2

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import requests
+
+API = "https://www.lostarkmarket.online/api/export-market-live/:region?ids=101301,101011"
+
+payload={}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+
+print(response.text)
