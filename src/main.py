e730e0c3611f5cf9543bf80a8abac3ed7e89a05b
Athlmyn <Athlmyn@Outlook.com>
2022-10-19 02:04:34 -0600

    Fixed

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import requests
 
-url = "https://www.lostarkmarket.online/api/export-market-live/region?ids=101301,101011"
+url = "https://www.lostarkmarket.online/api/export-market-live/North America East/?ids=101301,101011"
+#"region" : "North America East"
+payload={   
+            "items": "basic-oreha-fusion-material-2" }
+headers = {}
 
-payload={}
-headers = { }
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
